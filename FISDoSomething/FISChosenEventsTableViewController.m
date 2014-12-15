744f055f64dae011f029ef19cb418ed774d9eb32
ismailmustafa <ismailmustafa@rocketmail.com>
2014-12-15 09:33:23 -0500

    minor changes

--- FISDoSomething/FISChosenEventsTableViewController.m
+++ FISDoSomething/FISChosenEventsTableViewController.m
@@ -14,13 +14,12 @@
 #import "PopOverAnimation.h"
 #import "Campaign.h"
 #import "FISDataStore.h"
+#import "FISAppFont.h"
 
 @interface FISChosenEventsTableViewController () <FISEventSwipeViewControllerProtocol, DZNEmptyDataSetDelegate, DZNEmptyDataSetSource, UIViewControllerTransitioningDelegate>
 
-@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 @property (strong, nonatomic) NSMutableArray* eventsToDisplay;
 @property (strong, nonatomic) FISEventSwipeViewController *swipeVC;
-- (IBAction)segmentChanged:(id)sender;
 
 @end
 
@@ -67,7 +66,7 @@
     [self.navigationController.navigationBar
      setTitleTextAttributes:
      [NSDictionary dictionaryWithObjectsAndKeys:
-      [UIFont fontWithName:@"ProximaNovaA-Bold" size:23],
+      appFont(23),
       NSFontAttributeName, [UIColor whiteColor],
       NSForegroundColorAttributeName, nil]];
     self.navigationController.navigationBar.backgroundColor = [UIColor colorWithRed:77.0/255.0 green:43.0/255.0 blue:99.0/255.0 alpha:1.0];
@@ -151,39 +150,5 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
 
 }
-
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- } else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
 
 @end
